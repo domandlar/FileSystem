1ca7807623e3cce2889da321600de0b4ce62ccdd
Domagoj Andlar <domagoj.andlar@gmail.com>
2022-02-09 00:37:46 +0100

    Added validation to see if the folder exists in the "Create File" feature.

--- FileSysteemSolution/FileSystem.API/Services/Files/FileService.cs
+++ FileSysteemSolution/FileSystem.API/Services/Files/FileService.cs
@@ -1,23 +1,31 @@
 ﻿using FileSystem.API.Repositories.Files;
+using FileSystem.API.Repositories.Folders;
 
 namespace FileSystem.API.Services.Files
 {
     public class FileService : IFileService
     {
         private readonly IFileRepository _fileRepository;
+        private readonly IFolderRepository _folderRepository;
 
-        public FileService(IFileRepository fileRepository)
+        public FileService(IFileRepository fileRepository, IFolderRepository folderRepository)
         {
             _fileRepository = fileRepository;
+            _folderRepository = folderRepository;
         }
 
         public async Task<Entities.File> CreateFileAsync(Entities.File file)
         {
 
-            var fileExistsInFolder = await _fileRepository.CheckIfFileExistsInFolderAsync(file.Name, file.FolderId);
-            if (fileExistsInFolder)
+            var isFileExistsInFolder = await _fileRepository.CheckIfFileExistsInFolderAsync(file.Name, file.FolderId);
+            if (isFileExistsInFolder)
             {
                 throw new Exception("File already exists!");
+            }
+            var isFolderExists = await _folderRepository.CheckIfFolderExistsByIdAsync(file.FolderId);
+            if (!isFolderExists)
+            {
+                throw new Exception("Folder does not exist!");
             }
             var createdFile = await _fileRepository.CreateFileAsync(file);
             return createdFile;
